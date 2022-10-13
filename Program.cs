4d7d745ae1e579e5d585e167f394ced1e55579b5
Ollie.Mason <om343@student.aru.ac.uk>
2022-10-13 14:47:57 +0100

    bubble sort code

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿int[] bubbles = {3,1,5,2,4};
+int temp = 0; 
+for (int x = 0; x<bubbles.Length; x++)
+{
+    for (int i = 0; i<bubbles.Length-1;i++)
+    {
+        if (bubbles[i]<bubbles[i+1])
+        {
+            temp = bubbles[i];
+            bubbles[i] = bubbles[i+1];
+            bubbles[i+1] = temp;
+        }
+        
+    }
+
+}
+
+for (int i=0; i<5; i++)
+{
+        Console.WriteLine( bubbles[i]);
+}
+
+
+
+Console.ReadKey();
